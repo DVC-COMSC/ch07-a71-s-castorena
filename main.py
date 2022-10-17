1bcf1737ad184a2d22c6d67f9d53f5d95a05ef43
sarah castorena <ceanz@yahoo.com>
2022-10-17 14:31:55 -0700

    new

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 
-numbers = list(map(int, input().split()))
+numbers = list(map(int, input('Enter 10 numbers').split()))
+
 
 # ******************************
 # Make your Code
 # ******************************
+average = float(sum(numbers)/len(numbers))
+distance = []
+for i in range(len(numbers)):
+    dist = (abs(average - i))
+    distance.append(dist)
 
-
+for dist in distance:
+    print(f'{dist:.2f}', end =' ')
 # Use this statement to print out the list element. # Replace the variable 'dist' with your variable
-# print (f'{dist:.2f}', end=' ')
+
